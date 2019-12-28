c4c039d1c5cb2e2da581862176d096293cf7b204
Arvid Rudling <arru@users.noreply.github.com>
2019-12-28 12:02:48 +0100

    flag_duplicates script

---
+++ flag_duplicates.py
@@ -0,0 +1,65 @@
+import ImportUtils
+import plexapi.playlist
+
+CONFIGURATION = ImportUtils.get_configuration()
+
+# Duration match tolerance in ms
+duration_fuzziness = 2500
+
+plex = ImportUtils.PlexWrapper(CONFIGURATION)
+MUSIC_SECTION = plex.server.library.section('Music')
+plex_tracks = MUSIC_SECTION.searchTracks()
+
+print ("[INFO] clustering %d tracks" % len(plex_tracks))
+print ("[INFO] using %dms fuzziness" % duration_fuzziness)
+
+track_clusters = {}
+
+for plex_track in plex_tracks:
+    #grandparentRatingKey (str) – Unique key identifying album artist.
+    # media.bitrate
+    if not plex_track.title or not plex_track.artist():
+        continue
+
+    key = (plex_track.title, plex_track.artist(), int(plex_track.media[0].duration/duration_fuzziness))
+    if key in track_clusters:
+        track_clusters[key].append (plex_track)
+    else:
+        track_clusters[key] = [plex_track]
+
+uniques_counter = 0
+duplicate_groups_counter = 0
+
+print ("[INFO] finding highest bitrate in %d clusters" % len(track_clusters))
+
+duplicate_playlist_items = []
+
+for key, cluster in track_clusters.items():
+    if len(cluster) > 1:
+        highest_bitrate_value = 0
+        highest_bitrate_track = None
+        for track in cluster:
+            if track.media[0].bitrate > highest_bitrate_value:
+                highest_bitrate_value = track.media[0].bitrate
+                highest_bitrate_track = track
+
+        lower_bitrate_duplicates = filter(lambda t: t is not highest_bitrate_track, cluster)
+        duplicate_playlist_items.extend(lower_bitrate_duplicates)
+
+        # print ("Duplicate: %s\t(%d kbps)" % (str(key), highest_bitrate_track.media[0].bitrate))
+
+        duplicate_groups_counter += 1
+    else:
+        uniques_counter += 1
+
+playlist_title = "Duplicates (lower bitrates)"
+try:
+    duplicates_playlist = plex.server.playlist(playlist_title)
+    duplicates_playlist.delete()
+except plexapi.exceptions.NotFound:
+    pass
+
+plexapi.playlist.Playlist.create(plex.server, playlist_title, items=duplicate_playlist_items, section=MUSIC_SECTION)
+
+print ("[INFO] %d duplicate songs (%d total redundant tracks) identified" % (duplicate_groups_counter, len(duplicate_playlist_items)))
+print ("[INFO] %d singular/unique tracks" % uniques_counter)
